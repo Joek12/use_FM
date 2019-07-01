53c17d4d2b75d96f8c5b89c8f800f56c19aa824e
Joseph Kang <joseph.kang@utsouthwestern.edu>
2019-07-01 11:52:12 -0500

    add cmake-build-debug

---
+++ cmake-build-debug/CMakeFiles/use_FM.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/use_FM.dir/src/main.cpp.o"
+  "CMakeFiles/use_FM.dir/src/fastaReader.cpp.o"
+  "use_FM.pdb"
+  "use_FM"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/use_FM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
